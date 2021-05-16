3cb6dcc5298dee82dc8572e6f602c316b26a7037
JohnsonOtto <johannes.otto.me@gmail.com>
2021-05-16 16:35:53 +0200

    finished regiocup

--- regiocup-csv.py
+++ regiocup-csv.py
@@ -1,6 +1,8 @@
 import csv
 import pandas as pd
 import os
+import datetime as dt
+from time import perf_counter
 
 
 def readCSV(path):
@@ -10,14 +12,17 @@ def readCSV(path):
         return tmp[1:]
 
 
-def formatEventName(event):
-    groups = event.split(" ")
-    return " ".join(groups[len(groups) - 3 : len(groups) - 2])
+def formatEventCourse(filename):
+    groups = filename.split(" ")
+    course = groups[1][:2]
+    groups = groups[0].split("/")
+    event = groups[2]
+    return event, course
 
 
-def formatCourseName(course):
-    groups = course.split(" ")
-    return " ".join(groups[len(groups) - 2 : len(groups) - 1])
+def formatDate(datestr):
+    day, month, year = datestr.split("-")
+    return dt.date(int(year), int(month), int(day)).timetuple().tm_yday
 
 
 def time2sec(timestr):
@@ -38,167 +43,335 @@ def findNmax(array, n):
     return out
 
 
-def getFilesResults(files):
+# read files; filter mp, time = 0, not in timeframe; sort for first run
+def readFilesCleanData(files):
     results = []
-    for f in files:
-        event = [formatEventName(f)]
-        course = [formatCourseName(f)]
-        tmp = readCSV(f)
-
-        bestTime = time2sec("24:00:00")
-        for i in range(len(tmp)):
-            if time2sec(tmp[i][11]) < bestTime and tmp[i][12] == "0":
-                bestTime = time2sec(tmp[i][11])
-        for i in range(len(tmp)):
-            time = time2sec(tmp[i][11])
-            if time == 0 or tmp[i][12] == "0":
-                score = 0.0
-            else:
-                score = bestTime / time * 100
-                score = round(score, 2)
-            tmp[i].insert(25, score)
-            tmp[i] = course + event + tmp[i]
-
-        results = results + tmp
-        df = pd.DataFrame(results)
-
-    todrop = [2, 3, 4, 7, 8, 9, 10, 11, 12, 15, 17, 18, 19, 21, 22, 23, 24, 25] + list(
-        range(28, len(df.columns))
-    )
-    df.drop(columns=df.columns[todrop], inplace=True)
-    df.columns = [
-        "coursefile",
-        "event",
-        "name",
-        "surname",
-        "time",
-        "status",
-        "organisation",
-        "shortname",
-        "date",
-        "score",
-    ]
-    return df
-
-
-def cleanResults(df):
-    print(df)
-    df.drop_duplicates(inplace=True)
-    df.drop_duplicates(
-        subset=["surname", "name", "event", "coursefile"], keep="last", inplace=True
-    )
-    return df.values.tolist()
-
-
-def compactPersons(courseresult):
+    rest = []
     out = []
+    for f in files:
+        event, course = formatEventCourse(f)
+        tmp = readCSV(f)
+        for i in range(len(tmp)):
+            # print(tmp[i])
+            personDict = {
+                "info": {
+                    "surname": tmp[i][4],
+                    "lastname": tmp[i][3],
+                    "organisation": tmp[i][14],
+                },
+                "run": {
+                    "event": event,
+                    "course": course,
+                    "status": tmp[i][12],
+                    "starttime": 24 * 3600 * formatDate(tmp[i][24])
+                    + time2sec(tmp[i][9]),
+                    "startday": tmp[i][24],
+                    "time": tmp[i][11],
+                    "note": "",
+                },
+            }
+            if personDict in results:
+                continue
+            if personDict["run"]["time"] == "00:00":
+                continue
+
+            eventDict = list(
+                filter(lambda a: a["shortname"] == personDict["run"]["event"], events)
+            )
+            firstday = 24 * 3600 * formatDate(eventDict[0]["firstday"])
+            lastday = 24 * 3600 * (formatDate(eventDict[0]["lastday"]) + 1)
+
+            if (
+                personDict["run"]["status"] == "0"
+                and firstday < personDict["run"]["starttime"] < lastday
+            ):
+                results.append(personDict)
+            else:
+                personDict["run"]["score"] = 0.0
+                personDict["run"]["note"] = "Fehlstempel"
+                rest.append(personDict)
+
+    while len(results) > 0:
+        currentPerson = [results[0]]
+        identifier = (
+            results[0]["info"]["surname"]
+            + results[0]["info"]["lastname"]
+            + results[0]["info"]["organisation"]
+            + results[0]["run"]["event"]
+        )
+        for j in range(1, len(results)):
+            jdentifier = (
+                results[j]["info"]["surname"]
+                + results[j]["info"]["lastname"]
+                + results[j]["info"]["organisation"]
+                + results[j]["run"]["event"]
+            )
+            if identifier == jdentifier:
+                currentPerson.append(results[j])
+
+        if len(currentPerson) == 1:
+            out.append(currentPerson[0])
+            del results[0]
+        else:
+            earliest = 24 * 3600 * formatDate("31-12-4444") + time2sec("23:59:59")
+            toappend = "hee"
+            for person in currentPerson:
+                if person["run"]["starttime"] < earliest:
+                    if not toappend == "hee":
+                        results.remove(toappend)
+                        toappend["run"]["score"] = 0.0
+                        toappend["run"]["note"] = "Zweitlauf"
+                        rest.append(toappend)
+                    earliest = person["run"]["starttime"]
+                    toappend = person
+                else:
+                    results.remove(person)
+                    person["run"]["score"] = 0.0
+                    person["run"]["note"] = "Zweitlauf"
+                    rest.append(person)
+            out.append(toappend)
+            results.remove(toappend)
+    return out, rest
+
+
+# add score
+def calcScore(cup):
+    out = []
+    for event in events:
+        course = []
+        bestTime = time2sec("24:00:00")
+
+        for person in cup:
+            if event["shortname"] == person["run"]["event"]:
+                course.append(person)
+                # cup.remove(person)
+
+                if time2sec(person["run"]["time"]) < bestTime:
+                    bestTime = time2sec(person["run"]["time"])
+
+        for person in course:
+            score = bestTime / time2sec(person["run"]["time"]) * 100
+            person["run"]["score"] = round(score, 2)
+        out += course
+
+    return out
+
+
+# for results and rest, returns df
+def compactPersons(courseresult):
+    compacted = []
     while len(courseresult) > 0:
-        name = courseresult[0][3] + " " + courseresult[0][2]
         toappend = [
-            courseresult[0][0],
-            courseresult[0][2],
-            courseresult[0][3],
-            courseresult[0][6],
-            [courseresult[0][1], courseresult[0][8], courseresult[0][9]],
+            courseresult[0]["run"]["course"],
+            courseresult[0]["info"]["lastname"],
+            courseresult[0]["info"]["surname"],
+            courseresult[0]["info"]["organisation"],
+            [
+                courseresult[0]["run"]["event"],
+                courseresult[0]["run"]["startday"],
+                courseresult[0]["run"]["time"],
+                courseresult[0]["run"]["score"],
+                courseresult[0]["run"]["note"],
+            ],
         ]
 
-        for i in range(1, len(courseresult) - 1):
-            if name == courseresult[i][3] + " " + courseresult[i][2]:
+        i = 1
+        while True:
+            if i >= len(courseresult) or len(courseresult) == 1:
+                break
+            if courseresult[0]["info"] == courseresult[i]["info"]:
                 toappend.append(
-                    [courseresult[i][1], courseresult[i][8], courseresult[i][9]]
+                    [
+                        courseresult[i]["run"]["event"],
+                        courseresult[i]["run"]["startday"],
+                        courseresult[i]["run"]["time"],
+                        courseresult[i]["run"]["score"],
+                        courseresult[i]["run"]["note"],
+                    ]
                 )
                 del courseresult[i]
+            else:
+                i += 1
 
-        out.append(toappend)
+        compacted.append(toappend)
         del courseresult[0]
-    return out
 
-
-def sortEventAddScore(courseresult, order):
     out = []
-    for i in range(len(courseresult)):
-        toappend = [
-            courseresult[i][0],
-            courseresult[i][1],
-            courseresult[i][2],
-            courseresult[i][3],
-        ]
-        for event in order:
+    for i in range(len(compacted)):
+        toappend = [compacted[i][0], compacted[i][1], compacted[i][2], compacted[i][3]]
+        misPunch = False
+        # print(toappend)
+        for event in events:
             check = False
-            for j in range(4, len(courseresult[i])):
-                if courseresult[i][j][0] == event:
-                    toappend.append(courseresult[i][j][2])
+            for j in range(4, len(compacted[i])):
+                if compacted[i][j][0] == event["shortname"]:
                     check = True
+                    if compacted[i][j][4] == "Fehlstempel":
+                        misPunch = True
+                        toappend += [
+                            compacted[i][j][1],
+                            compacted[i][j][2],
+                            "0",
+                            compacted[i][j][4],
+                        ]
+                    else:
+                        toappend += [
+                            compacted[i][j][1],
+                            compacted[i][j][2],
+                            compacted[i][j][3],
+                            compacted[i][j][4],
+                        ]
             if not check:
-                toappend.append(0.0)
-
-        scores = toappend[4 : 4 + len(order)]
-        score = round(sum(findNmax(scores, 4)), 2)
+                toappend += ["----", "----", 0.0, ""]
+        if not misPunch:
+            scores = [toappend[6], toappend[10], toappend[14], toappend[18]]
+            score = round(sum(findNmax(scores, 4)), 2)
+        else:
+            score = 0.0
         toappend.append(score)
-
         out.append(toappend)
-    return out
+    return pd.DataFrame(out)
 
 
 if __name__ == "__main__":
-    inputDir = "input-csv"
-    eventOrder = ["MOL2021", "USC2021"]
-    eventNames = ["Magdeburger OL", "Ottos Wald-OL"]
-    headers = ["Bahn", "Nachname", "Vorname", "Verein"] + eventNames + ["Gesamt"]
-    finalHeaders = (
-        ["Bahn", "Platzierung", "Nachname", "Vorname", "Verein"]
-        + eventNames
-        + ["Gesamt"]
-    )
+    print("Start!")
+    t1 = perf_counter()
+
+    inputDir = "regiocup/csv"
+    outputDir = "regiocup"
+    events = [
+        {
+            "name": "Magdeburger OL",
+            "shortname": "MOL2021",
+            "firstday": "06-03-2021",
+            "lastday": "14-03-2021",
+        },
+        {
+            "name": "Ottos Wald-OL",
+            "shortname": "USC2021",
+            "firstday": "20-03-2021",
+            "lastday": "05-04-2021",
+        },
+        {
+            "name": "64. Kreismeisterschaft Quedlinburg",
+            "shortname": "KM64",
+            "firstday": "27-03-2021",
+            "lastday": "02-05-2021",
+        },
+        {
+            "name": "Wolfsburg",
+            "shortname": "WOB",
+            "firstday": "24-04-2021",
+            "lastday": "09-05-2021",
+        },
+    ]
+
+    headers = [
+        "Bahn",
+        "Platzierung",
+        "Nachname",
+        "Vorname",
+        "Verein",
+        "ESV Laufdatum",
+        "ESV Laufzeit",
+        "ESV Punkte",
+        "ESV Bemerkung",
+        "USC Laufdatum",
+        "USC Laufzeit",
+        "USC Punkte",
+        "USC Bemerkung",
+        "QLB Laufdatum",
+        "QLB Laufzeit",
+        "QLB Punkte",
+        "QLB Bemerkung",
+        "WOB Laufdatum",
+        "WOB Laufzeit",
+        "WOB Punkte",
+        "WOB Bemerkung",
+        "Punkte gesamt",
+    ]
 
     fileList = os.listdir(inputDir)
     for i in range(len(fileList)):
         fileList[i] = inputDir + "/" + fileList[i]
 
-    rawResults = getFilesResults(fileList)
-    cleanedList = cleanResults(rawResults)
+    cupResults, restResults = readFilesCleanData(fileList)
 
-    courses = [[], [], [], [], [], []]
+    coursesCup = [[], [], [], [], [], []]
+    coursesRest = [[], [], [], [], [], []]
 
-    for i in range(len(cleanedList)):
-        if cleanedList[i][0] == "KL" or :
-            courses[0].append(cleanedList[i])
-        if cleanedList[i][0] == "KS":
-            courses[1].append(cleanedList[i])
-        if cleanedList[i][0] == "ML":
-            courses[2].append(cleanedList[i])
-        if cleanedList[i][0] == "MS":
-            courses[3].append(cleanedList[i])
-        if cleanedList[i][0] == "LL":
-            courses[4].append(cleanedList[i])
-        if cleanedList[i][0] == "LS":
-            courses[5].append(cleanedList[i])
+    for i in range(len(cupResults)):
+        if cupResults[i]["run"]["course"] == "KL":
+            coursesCup[0].append(cupResults[i])
+        elif cupResults[i]["run"]["course"] == "KS":
+            coursesCup[1].append(cupResults[i])
+        elif cupResults[i]["run"]["course"] == "ML":
+            coursesCup[2].append(cupResults[i])
+        elif cupResults[i]["run"]["course"] == "MS":
+            coursesCup[3].append(cupResults[i])
+        elif cupResults[i]["run"]["course"] == "LL":
+            coursesCup[4].append(cupResults[i])
+        elif cupResults[i]["run"]["course"] == "LS":
+            coursesCup[5].append(cupResults[i])
 
-    total = []
-    for i in range(len(courses)):
-        courses[i] = compactPersons(courses[i])
-        courses[i] = sortEventAddScore(courses[i], eventOrder)
-        courses[i] = pd.DataFrame(
-            courses[i],
-            columns=headers,
-        )
-        courses[i].sort_values(by="Gesamt", ascending=False, inplace=True)
-        courses[i].reset_index(inplace=True, drop=True)
-        courses[i].insert(
-            loc=1, column="Platzierung", value=range(1, len(courses[i]) + 1)
-        )
-        courses[i] = pd.DataFrame(
-            [finalHeaders] + courses[i].values.tolist(), columns=finalHeaders
-        )
-        total += courses[i].values.tolist() + [" "]
+    for i in range(len(restResults)):
+        if restResults[i]["run"]["course"] == "KL":
+            coursesRest[0].append(restResults[i])
+        elif restResults[i]["run"]["course"] == "KS":
+            coursesRest[1].append(restResults[i])
+        elif restResults[i]["run"]["course"] == "ML":
+            coursesRest[2].append(restResults[i])
+        elif restResults[i]["run"]["course"] == "MS":
+            coursesRest[3].append(restResults[i])
+        elif restResults[i]["run"]["course"] == "LL":
+            coursesRest[4].append(restResults[i])
+        elif restResults[i]["run"]["course"] == "LS":
+            coursesRest[5].append(restResults[i])
 
     writer = pd.ExcelWriter("regiocup/Auswertung Regiocup.xlsx", engine="xlsxwriter")
 
-    pd.DataFrame(total).to_excel(writer, sheet_name="Gesamt", index=False, header=False)
-    for i in range(len(courses)):
-        courses[i].to_excel(
-            writer, sheet_name=courses[i].iloc[-1, 0], index=False, header=False
+    total = []
+    for i in range(len(coursesCup)):
+        coursesCup[i] = calcScore(coursesCup[i])
+        coursesCup[i] = compactPersons(coursesCup[i])
+        coursesCup[i].sort_values(by=20, ascending=False, inplace=True)
+        coursesCup[i].reset_index(inplace=True, drop=True)
+        coursesCup[i].insert(
+            loc=1, value=range(1, len(coursesCup[i]) + 1), column="Platzierung"
         )
+        coursesCup[i].replace(to_replace=0.0, value="----", inplace=True)
+        coursesCup[i].columns = headers
 
+        coursesRest[i] = compactPersons(coursesRest[i])
+        coursesRest[i].sort_values(by=1, inplace=True)
+        coursesRest[i].reset_index(inplace=True, drop=True)
+        coursesRest[i].insert(loc=1, value="----", column="Platzierung")
+        coursesRest[i].replace(to_replace=0.0, value="----", inplace=True)
+        coursesRest[i].columns = headers
+
+        final = (
+            [headers]
+            + coursesCup[i].values.tolist()
+            + [" "]
+            + coursesRest[i].values.tolist()
+        )
+        final = pd.DataFrame(final)
+        final.columns = headers
+        sheetName = final.iloc[-1, 0]
+        final.to_excel(writer, sheet_name=sheetName, index=False, header=False)
+
+        worksheet = writer.sheets[sheetName]
+        worksheet.set_column(1, 3, 10)
+        worksheet.set_column(4, 4, 20)
+        worksheet.set_column(5, 21, 14)
+
+        total += coursesCup[i].values.tolist() + [" "]
+
+    total = pd.DataFrame([headers] + total)
+    total.to_excel(writer, sheet_name="Gesamt", index=False, header=False)
     writer.save()
-    print("Done!")
+
+    t2 = perf_counter()
+    t = round(t2 - t1, 3)
+    print("Done! " + str(t) + "s")
+    # input("Press Enter to exit...")
